e3edf43c263c85de453184a4485ed6b7741d2c1f
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-05-15 18:41:55 -0500

    feat: Consultar API

--- js/API.js
+++ js/API.js
@@ -1,21 +1,18 @@
-class UI {
-    constructor() {
+class API {
+   
 
-         // Iniciar el mapa
-         this.mapa = this.inicializarMapa();
+    async obtenerDatos() {
 
-    }
+        const total = 11000;
 
-    inicializarMapa() {
-         // Inicializar y obtener la propiedad del mapa
-         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
-         const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-         L.tileLayer(
-             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-             attribution: '&copy; ' + enlaceMapa + ' Contributors',
-             maxZoom: 18,
-             }).addTo(map);
-         return map;
+         // Obtener los datos desde la api
+         const datos = await fetch(`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
+         
+        //retornar datos como json
+        const respuestaJSON = await datos.json();
 
+        return{
+            respuestaJSON
+        }
     }
 }
